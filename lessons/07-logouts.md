fe86709bbe563f7798320625c5b74e63ae741884
Jen Andre <jandre@gmail.com>
2015-05-04 19:16:38 -0400

    Add logouts

---
+++ lessons/07-logouts.md
@@ -0,0 +1,76 @@
+# Lesson 7 - Notify When Users Logout
+
+## Goals
+
+We know when users login, we send and receive chatroom messages.. but how do we know when users logout?  
+
+Let's write some code to handle disconnects and display a message
+`*** [username] disconnected` when a socket disconnects.
+
+How will we accomplish this?  With more channels, of course!
+
+1. Whenever a ChatUser fails to read/write to its connection socket, we will send a message on the `ChatRoom.disconnect` channel to notify the `ListenForMessages` loop
+that the socket has been disconnected.
+1. When such a message is received by the `ChatRoom`, remove it from the `users` map and broadcast a message to the rest of the clients saying it's disconnected.
+
+## Steps
+
+1. Find the `ChatRoom.Logout` function. Add some code that does the following:
+  1. :star2: Send the provided `username` to the `cr.disconnects` channel.
+
+  [Stuck on any of the steps above? Ask your TA, or see the solution!](code/07-logouts/chat.go)
+
+1. Update the `ListenForMessages` loop to handle messages on `cr.disconnects`:
+  1. :star2: Add a `case` statement that reads the `username` from `cr.disconnects`
+  1. :star2: if the `username` is in the `cr.users` map:
+    1. :star2: call `Close()` on the `ChatUser` object and remove it from the map.
+    1. :star2: call `Broadcast` to send a message: _`*** [username] has disconnected`_
+
+  [Stuck on any of the steps above? Ask your TA, or see the solution!](code/07-logouts/chat.go)
+
+1. Now that we have `Logout` implemented, let's call `Logout` whenever the ChatUser read and write loops
+have errors reading or writing from the socket. 
+
+  1. Update `ChatUser.ReadIncomingMessages` so that it does the following:
+
+    1. :star2: If cu.ReadLine() returns an error, do not write a message to the `chatroom.incoming` queue. 
+    Instead, call `chatroom.Logout` with the current username.
+    1. :star2: Add some logic that checks if `cu.disconnect` is set, then exit the loop.
+  
+    [Stuck on any of the steps above? Ask your TA, or see the solution!](code/07-logouts/chat.go)
+
+  1. Update `ChatUser.WriteOutgoingMessages` so that: 
+    
+    1. :star2: If cu.ReadLine() returns an error, do not write a message to the queue. Instead,
+    call `chatroom.Logout` with the current username.
+    put the current username on the `chatroom.disconnects` channel.
+    1. :star2: Add some logic that checks if `cu.disconnect` is set, then exit the loop.
+  
+    [Stuck on any of the steps above? Ask your TA, or see the solution!](code/07-logouts/chat.go)
+
+1. Finally, let's implement `ChatUser.Close`.  This will set the `disconnect` variable and close the socket.
+
+  ```go
+  func (cu *ChatUser) Close() {
+  	// TODO: close the socket
+  }
+  ```
+  1. :star2: In `Close()`, set `ChatUser.disconnect = true`
+  1. :star2: call Close() on the `ChatUser.conn` object
+  
+  [Stuck on any of the steps above? Ask your TA, or see the solution!](code/07-logouts/chat.go)
+
+1. Now when a user disconnects, you'll see a disconnect message!
+  ```bash
+   $ nc localhost 6677
+   Welcome to Jen's chat server!
+   Please enter your username: funcuddles
+   Welcome, funcuddles
+   *** funcuddles just joined the chatroom
+   *** bob just joined the chatroom
+   hello this is dog
+   [funcuddles] hello this is dog
+   [bob] bye funcuddles, I gotta go!
+   *** bob has disconnected
+   ```
+
---
+++ lessons/code/07-logouts/chat.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net"
+)
+
+//
+// ChatRoom is the main chatroom data structure.
+//
+// `users` contains connected ChatUser connections.
+// `incoming` receives incoming messages from ChatUser connections.
+// `joins` receives incoming new ChatUser connections.
+// `disconnects` receives disconnect notifications.
+//
+type ChatRoom struct {
+	users       map[string]*ChatUser
+	incoming    chan string
+	joins       chan *ChatUser
+	disconnects chan string
+}
+
+//
+// NewChatRoom() will create a new chatroom.
+//
+func NewChatRoom() *ChatRoom {
+	return &ChatRoom{
+		users:       make(map[string]*ChatUser),
+		incoming:    make(chan string),
+		joins:       make(chan *ChatUser),
+		disconnects: make(chan string),
+	}
+}
+
+func (cr *ChatRoom) ListenForMessages() {
+	go func() {
+		for {
+			select {
+			case msg := <-cr.incoming:
+				cr.Broadcast(msg)
+			case user := <-cr.joins:
+				cr.users[user.username] = user
+				cr.Broadcast("*** " + user.username + " just joined the chatroom")
+			case username := <-cr.disconnects:
+				// perform the logout by removing
+				// the username from the `users` map if it exists.
+				// Also call Close() on the user object.
+				if cr.users[username] != nil {
+					cr.users[username].Close()
+					delete(cr.users, username)
+					cr.Broadcast("*** " + username + " disconnected")
+				}
+			}
+		}
+	}()
+}
+
+func (cr *ChatRoom) Logout(username string) {
+	cr.disconnects <- username
+}
+
+func (cr *ChatRoom) Join(conn net.Conn) {
+	user := NewChatUser(conn)
+	if user.Login(cr) == nil {
+		cr.joins <- user
+	}
+}
+
+func (cr *ChatRoom) Broadcast(msg string) {
+	for _, user := range cr.users {
+		user.Send(msg)
+	}
+}
+
+//
+// ChatUser contains information for the connected user.
+//
+// `conn` is the socket.
+// `disconnect` indicates whether or not the socket is disconnected.
+// `username` is the chat username.
+// `outgoing` is a channel with all pending outgoing messages
+// to be written to the socket.
+// `reader` is the buffered socket read stream.
+// `writer` is the buffered socket write stream.
+//
+type ChatUser struct {
+	conn       net.Conn
+	disconnect bool
+	username   string
+	outgoing   chan string
+	reader     *bufio.Reader
+	writer     *bufio.Writer
+}
+
+func NewChatUser(conn net.Conn) *ChatUser {
+	writer := bufio.NewWriter(conn)
+	reader := bufio.NewReader(conn)
+
+	cu := &ChatUser{
+		conn:       conn,
+		disconnect: false,
+		reader:     reader,
+		writer:     writer,
+		outgoing:   make(chan string),
+	}
+
+	return cu
+}
+
+func (cu *ChatUser) ReadIncomingMessages(chatroom *ChatRoom) {
+	go func() {
+		for {
+			line, err := cu.ReadLine()
+			if cu.disconnect {
+				break
+			}
+			if err != nil {
+				chatroom.Logout(cu.username)
+				break
+			}
+			if line != "" {
+				chatroom.incoming <- ("[" + cu.username + "] " + line)
+			}
+		}
+	}()
+}
+
+func (cu *ChatUser) WriteOutgoingMessages(chatroom *ChatRoom) {
+	go func() {
+		for {
+			data := <-cu.outgoing
+			if cu.disconnect {
+				break
+			}
+			data = data + "\n"
+			err := cu.WriteString(data)
+			if err != nil {
+				chatroom.Logout(cu.username)
+				break
+			}
+		}
+	}()
+}
+
+func (cu *ChatUser) Login(chatroom *ChatRoom) error {
+	var err error
+	cu.WriteString("Welcome to Jen's chat server!\n")
+	cu.WriteString("Please enter your username: ")
+
+	cu.username, err = cu.ReadLine()
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("User logged in:", cu.username)
+
+	cu.WriteString("Welcome, " + cu.username + "\n")
+
+	cu.WriteOutgoingMessages(chatroom)
+	cu.ReadIncomingMessages(chatroom)
+	return nil
+}
+
+func (cu *ChatUser) ReadLine() (string, error) {
+	bytes, _, err := cu.reader.ReadLine()
+	str := string(bytes)
+	return str, err
+}
+
+func (cu *ChatUser) WriteString(msg string) error {
+	_, err := cu.writer.WriteString(msg)
+	if err != nil {
+		return err
+	}
+	return cu.writer.Flush()
+}
+
+func (cu *ChatUser) Send(msg string) {
+	cu.outgoing <- msg
+}
+
+func (cu *ChatUser) Close() {
+	cu.disconnect = true
+	cu.conn.Close()
+}
+
+//
+// main will create a socket, bind to port 6677,
+// and loop while waiting for connections.
+//
+// When it receives a connection it will pass it to
+// `chatroom.Join()`.
+//
+func main() {
+	log.Println("Chat server starting!")
+	chatroom := NewChatRoom()
+
+	// This binds the socket to TCP port 6677
+	// on all interfaces.
+	listener, err := net.Listen("tcp", ":6677")
+
+	chatroom.ListenForMessages()
+
+	if err != nil {
+		log.Fatal("Unable to bind to 6677", err)
+	}
+
+	// Accept a connection, and print out the remote
+	// address so we know who has connected.
+	for {
+		conn, _ := listener.Accept()
+		log.Println("Connection joined.", conn.RemoteAddr())
+
+		// run this in a goroutine so more than one thing
+		// can connect
+		go chatroom.Join(conn)
+	}
+}
